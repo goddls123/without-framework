428caf429392db9614889095f91464b97efe576b
goddls123 <coolch0916@gmail.com>
2021-10-19 08:33:23 +0900

    add index create dom

--- http/index.js
+++ http/index.js
@@ -1,8 +1,5 @@
-//render
+import todos from "./todos.js";
 
-render = () => {
-    window.requestAnimationFrame(() => {});
-};
 const NEW_TODO_TEXT = "A simpel todo Element";
 //dom 생성
 const printResult = (action, result) => {
@@ -16,5 +13,46 @@ const printResult = (action, result) => {
     document.querySelector("div").appendChild(node);
 };
 //event
+
+const onListClick = async () => {
+    const data = await todos.list();
+    printResult("get", data);
+};
+
+const onUpdateClick = async () => {
+    const list = await todos.list();
+    const { id } = list[0];
+    const newTodo = { id, completed: true };
+    const data = await todos.update(newTodo);
+
+    printResult("update", data);
+};
+
+const onAddClick = async () => {
+    const todo = await todos.create(NEW_TODO_TEXT);
+
+    printResult("add", todo);
+};
+
+const onDeleteClick = async () => {
+    const list = await todos.list();
+    const { id } = list[0];
+    const data = await todos.deleteTodo(id);
+
+    printResult("delete", data);
+};
+
+document
+    .querySelector("button[data-list]")
+    .addEventListener("click", onListClick);
+document
+    .querySelector("button[sdata-add]")
+    .addEventListener("click", onAddClick);
+document
+    .querySelector("button[data-delete]")
+    .addEventListener("click", onDeleteClick);
+document
+    .querySelector("button[data-update]")
+    .addEventListener("click", onUpdateClick);
 
 //보여주기
